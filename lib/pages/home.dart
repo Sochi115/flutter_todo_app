309cadd462c7fed08936aad82767f250003e489d
Chiv Sochivoath <chiv.sochivoath@gmail.com>
2022-12-11 18:03:35 +0700

    Added delete all done button and fix scroll issue

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,26 +3,42 @@ import '../constants/colors.dart';
 import '../models/todo.dart';
 import '../widgets/todo_item.dart';
 
-class Home extends StatelessWidget {
-  Home({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
-  final todosList = Todo.todoList();
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _todosList = Todo.todoList();
+  List<Todo> _foundTodo = [];
+  final _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    _foundTodo = _todosList;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: tdBGColor,
-        appBar: titleAppBar(),
-        body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(children: [
-            const SearchBar(),
-            Expanded(
-              child: ListView(
+      backgroundColor: tdBGColor,
+      appBar: titleAppBar(),
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Column(children: [
+              searchBar(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
+                    alignment: Alignment.centerLeft,
                     margin: const EdgeInsets.only(
-                      top: 40,
+                      top: 20,
                       bottom: 20,
                     ),
                     child: const Text(
@@ -31,13 +47,94 @@ class Home extends StatelessWidget {
                           TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
                     ),
                   ),
-                  for(Todo todo in todosList)
-                    TodoItem(todoItem: todo,)
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: tdBlue,
+                        elevation: 10,
+                      ),
+                      onPressed: () {
+                        _deleteAllDone();
+                      },
+                      child: const Text("Delete Done"))
                 ],
               ),
-            )
-          ]),
-        ));
+              Expanded(
+                child: ListView(
+                  children: [
+                    for (Todo todo in _foundTodo.reversed)
+                      TodoItem(
+                        todoItem: todo,
+                        onTodoChanged: _handleToDoChange,
+                        onDeleteItem: _deleteTodoItem,
+                      ),
+                    Container(
+                        margin: const EdgeInsets.only(
+                      top: 70,
+                    ))
+                  ],
+                ),
+              )
+            ]),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0)
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    controller: _todoController,
+                    decoration: const InputDecoration(
+                      hintText: 'Add new todo item',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: tdBlue,
+                    minimumSize: const Size(60, 60),
+                    elevation: 10,
+                  ),
+                  child: const Text(
+                    "+",
+                    style: TextStyle(fontSize: 40),
+                  ),
+                  onPressed: () {
+                    _addTodoItem(_todoController.text);
+                  },
+                ),
+              )
+            ]),
+          )
+        ],
+      ),
+    );
   }
 
   AppBar titleAppBar() {
@@ -49,21 +146,60 @@ class Home extends StatelessWidget {
           color: tdBlack, height: 2, fontSize: 25, fontWeight: FontWeight.bold),
     );
   }
-}
 
-class SearchBar extends StatelessWidget {
-  const SearchBar({
-    Key? key,
-  }) : super(key: key);
+  void _handleToDoChange(Todo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
 
-  @override
-  Widget build(BuildContext context) {
+  void _deleteTodoItem(String id) {
+    setState(() {
+      _todosList.removeWhere((item) => item.id == id);
+    });
+  }
+
+  void _addTodoItem(String userTodo) {
+    setState(() {
+      _todosList.add(Todo(
+          id: DateTime.now().millisecondsSinceEpoch.toString(),
+          todoText: userTodo));
+    });
+
+    _todoController.clear();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Todo> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = _todosList;
+    } else {
+      results = _todosList
+          .where((element) => element.todoText!
+              .toLowerCase()
+              .contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+
+    setState(() {
+      _foundTodo = results;
+    });
+  }
+
+  void _deleteAllDone() {
+    setState(() {
+      _todosList.removeWhere((element) => element.isDone);
+    });
+  }
+
+  Widget searchBar() {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(30)),
-      child: const TextField(
-        decoration: InputDecoration(
+      child: TextField(
+        onChanged: (value) => _runFilter(value),
+        decoration: const InputDecoration(
           contentPadding: EdgeInsets.all(0),
           prefixIcon: Icon(
             Icons.search,
--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -5,42 +5,51 @@ import '../models/todo.dart';
 
 class TodoItem extends StatelessWidget {
   final Todo todoItem;
-  const TodoItem({super.key, required this.todoItem});
+  final onTodoChanged;
+  final onDeleteItem;
+  const TodoItem(
+      {super.key,
+      required this.todoItem,
+      this.onTodoChanged,
+      this.onDeleteItem});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 20),
-      child: ListTile(
-        onTap: () {
-          print('Tapped list tile');
-        },
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        tileColor: Colors.white,
-        leading: const Icon(
-          Icons.check_box,
-          color: tdBlue,
-        ),
-        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-        title: Text(
-          todoItem.todoText!,
-          style: const TextStyle(
-              color: tdBlack,
-              fontSize: 16,
-              decoration: TextDecoration.lineThrough),
-        ),
-        trailing: Container(
-          height: 35,
-          width: 35,
-          decoration: BoxDecoration(
-              color: tdRed, borderRadius: BorderRadius.circular(10)),
-          child: IconButton(
-            color: Colors.white,
-            iconSize: 18,
-            icon: const Icon(Icons.delete),
-            onPressed: () {
-              print('Tapped delete button');
-            },
+    return Material(
+      color: tdBGColor,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 20),
+        child: ListTile(
+          onTap: () {
+            onTodoChanged(todoItem);
+          },
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          tileColor: Colors.white,
+          leading: Icon(
+            todoItem.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+            color: tdBlue,
+          ),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          title: Text(
+            todoItem.todoText!,
+            style: TextStyle(
+                color: tdBlack,
+                fontSize: 16,
+                decoration: todoItem.isDone ? TextDecoration.lineThrough : null),
+          ),
+          trailing: Container(
+            height: 35,
+            width: 35,
+            decoration: BoxDecoration(
+                color: tdRed, borderRadius: BorderRadius.circular(10)),
+            child: IconButton(
+              color: Colors.white,
+              iconSize: 18,
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                onDeleteItem(todoItem.id);
+              },
+            ),
           ),
         ),
       ),
